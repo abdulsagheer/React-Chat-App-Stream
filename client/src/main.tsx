5b88d80676234f908ac559f0894d1106cdc792ba
abdulsagheer <abdulsagheeras29@gmail.com>
2023-01-25 19:16:05 +0530

    Implemented Logic for Chatting

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "stream-chat-react/dist/css/index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+	<React.StrictMode>
+		<QueryClientProvider client={queryClient}>
+			<RouterProvider router={router} />
+		</QueryClientProvider>
+	</React.StrictMode>
+);
+
--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,13 @@
 import { config } from "dotenv";
+import { userRoutes } from "./routes/users";
 import fastify from "fastify";
+import cors from "@fastify/cors";
 config();
 const port = process.env.PORT || 5000;
-
 const app = fastify();
+
+app.register(cors, { origin: process.env.CLIENT_URL });
+app.register(userRoutes);
 
 app.listen(port, () => {
 	console.log("Server listening on port: " + port);
